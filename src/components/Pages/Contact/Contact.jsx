ef27e001e9841b85e68ec89226721ed8e0e11519
juan camilo grajales osorio <osorio7980@gmail.com>
2022-05-29 20:43:16 -0500

    generando contactenos

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import { Header } from '../../Layouts/Header/Header'
-
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+
+  const sendEmail=(event)=>{
+
+    event.preventDefault();
+
+    emailjs.sendForm('service_z8t442f','template_y651ugh',event.target,'WaN1WUEZKzqPq5FmN')
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error))
+
+  }
+
   return (
     <div className='container'>
         <Header></Header>
         <div className='container1'>
           <h2>Contact</h2>
-          <div className='formulario'>
+          <form className='formulario' onSubmit={sendEmail}>
             <label>Name</label>
-            <input className='input' type="text" />
+            <input className='input' type="text" name='user_name'/>
             <hr className='tamano'/>
             <label>Email</label>
-            <input className='input' type="correo" />
+            <input className='input' type="correo" name='user_email'/>
             <hr className='tamano'/>
             <label>Message</label>
-            <textarea className='inputarea' type="text" />
+            <textarea className='inputarea' name='user_message' />
             <hr className='tamano'/>
             <button className="boton">Send</button>
-          </div>
+          </form>
         </div>
     </div>
   )
